98fa199d826a6f67366e4f5a10037be9b638a804
yumin-jung <jungym887@naver.com>
2025-06-11 15:13:36 +0900

    Initial Commit

---
+++ SwiftUI-TCA/SwiftUI_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TCAApp.swift
+//  SwiftUI-TCA
+//
+//  Created by yumin on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
