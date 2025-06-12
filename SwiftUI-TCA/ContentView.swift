9a2ad61873a71947278af15871c06d28a269eb83
yumin-jung <jungym887@naver.com>
2025-06-12 18:35:20 +0900

    feat: add CounterView

--- SwiftUI-TCA/ContentView.swift
+++ SwiftUI-TCA/ContentView.swift
@@ -2,21 +2,47 @@
 //  ContentView.swift
 //  SwiftUI-TCA
 //
-//  Created by yumin on 6/11/25.
+//  Created by yumin on 6/12/25.
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    NavigationLink("Basics") {
+                        Demo(store: Store(initialState: Counter.State()) { Counter() }) { store in
+                            CounterDemoView(store: store)
+                        }
+                    }
+                } header: {
+                  Text("Getting started")
+                }
+            }
+            .navigationTitle("TCA")
         }
-        .padding()
     }
+}
+
+/// This wrapper provides an "entry" point into an individual demo that can own a store.
+struct Demo<State, Action, Content: View>: View {
+  @SwiftUI.State var store: Store<State, Action>
+  let content: (Store<State, Action>) -> Content
+
+  init(
+    store: Store<State, Action>,
+    @ViewBuilder content: @escaping (Store<State, Action>) -> Content
+  ) {
+    self.store = store
+    self.content = content
+  }
+
+  var body: some View {
+    content(store)
+  }
 }
 
 #Preview {
